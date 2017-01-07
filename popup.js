7473e89f5bf7e32cb611cf23cc87fd85f544ab83
Deepak Ranganathan <Deepak Ranganathan>
2017-01-07 16:52:20 -0600

    Added support to fetch and render download link in JSON format

--- popup.js
+++ popup.js
@@ -1,118 +1,132 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
+// Get the current URL.
+// 	callback - called when the URL of the current tab is found
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+	var queryInfo = {
+		active: true,
+		currentWindow: true
+	};
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+	chrome.tabs.query(queryInfo, function(tabs) {
+		var url = tabs[0].url;
+		console.assert(typeof url == 'string', 'tab.url should be a string');
+		callback(url);
+	});
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
+// CORS request. For future purposes
+//	method - HTTP method
+//	url    - URL requested
+function createCORSRequest(method, url) {
+	var xhr = new XMLHttpRequest();
+	if ("withCredentials" in xhr) {
+		// Check if the XMLHttpRequest object has a "withCredentials" property.
+		// "withCredentials" only exists on XMLHTTPRequest2 objects.
+		xhr.open(method, url);
+	} else if (typeof XDomainRequest != "undefined") {
+		// Otherwise, check if XDomainRequest.
+		// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+		xhr = new XDomainRequest();
+		xhr.open(method, url);
+	} else {
+		// Otherwise, CORS is not supported by the browser.
+		xhr = null;
+	}
+	return xhr;
+}
+
+// converts and downloads video. For future purposes
+//	url - URL of video
+//	callback - called when the conversion is done
+//	errorCallback - called when there is an error
+function convertVideo(url, callback, errorCallback) {
+	var downloadUrl = 'http://www.youtubeinmp3.com/fetch/?video='+encodeURIComponent(url);
+
+	var xhr = createCORSRequest('GET', downloadUrl);
+	if (!xhr) {
+		errorCallback('CORS not supported');
+	}
+	xhr.overrideMimeType("application/octet-stream");
+	xhr.responseType = "arraybuffer";
+	xhr.onload = function() {
+		var bb = new (window.BlobBuilder || window.WebKitBlobBuilder)();
+		var res = xhr.response;
+		if (!res) {
+			errorCallback('No response from conversion server');
+			return;
+		}
+		var byteArray = new Uint8Array(res);
+		bb.append(byteArray.buffer);
+		var blob = bb.getBlob("application/octet-stream");
+		var iframe = document.createElement("iframe");
+		iframe.style.display = "none";
+		iframe.src = window.webkitURL.createObjectURL(blob);
+		document.body.appendChild(iframe);
+
+		callback(imageUrl, width, height);
+	};
+	xhr.onerror = function() {
+		errorCallback('Network error');
+	};
+	xhr.send();
+}
+
+// fetches the download link and title in JSON format
+//	url - URL of video
+//	callback - called when the conversion is done
+//	errorCallback - called when there is an error
+function getLink(url, callback, errorCallback) {
+	var downloadUrl = 'http://www.youtubeinmp3.com/fetch/?format=json&video='+encodeURIComponent(url);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', downloadUrl);
+	xhr.responseType = 'json';
+	xhr.onload = function() {
+		// Parse and process the response
+		var res = xhr.response;
+		if (res===null) {
+			errorCallback('No response from Google Image search!');
+			return;
+		}
+		var videoTitle = res.title;
+		var videoLink = res.link;
+		callback(videoTitle, videoLink);
+	};
+	xhr.onerror = function() {
+		errorCallback('Network error.');
+	};
+	xhr.send();
 }
 
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
+function renderLink(linkText) {
+	var ln = document.getElementById('downloadlink');
+	ln.href = linkText;
+	ln.onclick = function () {
+		chrome.tabs.create({active: true, url: linkText});
+	}
+}
+
+function hideLink() {
+	document.getElementById('downloadlink').style.visibility = 'hidden';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus(url);
+    getCurrentTabUrl( function(url) {
+		if(url.includes('www.youtube.com/watch')) {
+			renderStatus('Downloading');
 
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
-  });
+			getLink(url, function(title,link) {
+				renderStatus(title);
+				renderLink(link);
+			}, function(errorMessage) {
+				renderStatus('Cannot convert video. ' + errorMessage);
+			});
+		} else  {
+			renderStatus('Not a youtube page. Can\'t download mp3');
+			hideLink();
+		}
+	});
 });
